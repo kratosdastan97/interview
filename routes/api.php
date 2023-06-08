6df0a191749e7c7f68b1ba2c5198e171f72a3f92
cristoferprodooh <cristoferg@prodooh.com>
2023-06-07 20:54:47 -0600

    [ADD] add routes for customers

--- routes/api.php
+++ routes/api.php
@@ -22,3 +22,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('/login', [AuthController::class, 'login']);
 
+Route::resource('customers', 'CustomerController');
+
+
