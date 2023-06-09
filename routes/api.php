14757edadb1ed5edec64d5398b489c5b438525c0
cristoferprodooh <cristoferg@prodooh.com>
2023-06-09 15:44:22 -0600

    [ADD] add passport and fix routes

--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('/login', [AuthController::class, 'login']);
 
-Route::resource('customers', 'CustomerController');
+Route::resource('customers', 'CustomerController')->except('update');
 
 
